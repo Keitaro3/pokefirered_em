79be0e546bd8792835c8e159a3dfc17fd371c2d9
Keitaro3 <110940628+Keitaro3@users.noreply.github.com>
2022-08-18 16:16:10 -0400

    Updated sound engine to use M4a projects
    Replaced the wildly inaccurate implementation of the sound engine with a version of Gamefreak's own M4A project. Audio can now all be handled through M4A ini files, which is the only sane and intuitive way anyone SHOULD be doing this. Running "tony.bat" in the M4A project folder will make the magic happen. You're welcome.

---
+++ sound/SndMonDat.s
@@ -0,0 +1,816 @@
+	.section .rodata
+	.align	2
+
+	.global	app_song_table
+app_song_table:
+	.short	 0, 90
+	.short	190, 100
+	.short	190, 100
+	.short	190, 100
+	.short	190, 100
+	.short	190, 80
+	.short	190, 110
+	.short	190, 80
+	.short	190, 80
+	.short	190, 120
+	.short	190, 100
+	.short	191, 90
+	.short	190, 110
+	.short	190, 110
+	.short	190, 110
+	.short	190, 100
+	.short	190, 110
+	.short	190, 90
+	.short	191, 95
+	.short	190, 75
+	.short	191, 110
+	.short	190, 60
+	.short	190, 110
+	.short	190, 70
+	.short	190, 110
+	.short	190, 80
+	.short	190, 120
+	.short	190, 80
+	.short	190, 90
+	.short	190, 100
+	.short	190, 110
+	.short	190, 120
+	.short	190, 110
+	.short	190, 127
+	.short	191, 90
+	.short	191, 100
+	.short	190, 100
+	.short	191, 110
+	.short	191, 100
+	.short	190, 90
+	.short	190, 90
+	.short	190, 90
+	.short	191, 90
+	.short	191, 127
+	.short	191, 127
+	.short	190, 127
+	.short	191, 110
+	.short	191, 90
+	.short	191, 80
+	.short	191, 100
+	.short	191, 100
+	.short	191, 100
+	.short	191, 100
+	.short	190, 100
+	.short	191, 120
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 20
+	.short	191, 95
+	.short	190, 90
+	.short	190, 90
+	.short	191, 105
+	.short	191, 105
+	.short	191, 105
+	.short	191, 110
+	.short	191, 110
+	.short	190, 110
+	.short	191, 100
+	.short	191, 80
+	.short	191, 80
+	.short	191, 100
+	.short	191, 100
+	.short	191, 80
+	.short	191, 80
+	.short	191, 110
+	.short	191, 110
+	.short	191, 100
+	.short	190, 100
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 105
+	.short	190, 90
+	.short	191, 90
+	.short	191, 70
+	.short	191, 70
+	.short	191, 110
+	.short	191, 90
+	.short	191, 90
+	.short	191, 95
+	.short	190, 100
+	.short	191, 90
+	.short	191, 100
+	.short	191, 110
+	.short	191, 100
+	.short	190, 40
+	.short	190, 90
+	.short	190, 100
+	.short	190, 100
+	.short	190, 100
+	.short	191, 120
+	.short	191, 110
+	.short	191, 110
+	.short	190, 100
+	.short	191, 105
+	.short	191, 120
+	.short	191, 100
+	.short	191, 100
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 95
+	.short	191, 110
+	.short	191, 95
+	.short	191, 110
+	.short	191, 120
+	.short	191, 110
+	.short	191, 105
+	.short	191, 105
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 100
+	.short	191, 110
+	.short	191, 120
+	.short	191, 90
+	.short	191, 110
+	.short	191, 100
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 105
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 105
+	.short	191, 110
+	.short	191, 110
+	.short	191, 90
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 115
+	.short	191, 115
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 100
+	.short	191, 120
+	.short	191, 95
+	.short	191, 100
+	.short	191, 95
+	.short	191, 115
+	.short	191, 90
+	.short	191, 115
+	.short	191, 125
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 120
+	.short	191, 110
+	.short	191, 115
+	.short	191, 115
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 100
+	.short	191, 100
+	.short	191, 110
+	.short	191, 120
+	.short	191, 65
+	.short	191, 90
+	.short	191, 127
+	.short	191, 120
+	.short	191, 110
+	.short	191, 110
+	.short	191, 105
+	.short	191, 100
+	.short	191, 110
+	.short	191, 95
+	.short	191, 95
+	.short	191, 110
+	.short	191, 100
+	.short	191, 90
+	.short	191, 95
+	.short	191, 100
+	.short	191, 110
+	.short	191, 100
+	.short	191, 100
+	.short	191, 90
+	.short	191, 110
+	.short	191, 110
+	.short	191, 115
+	.short	191, 100
+	.short	191, 110
+	.short	191, 127
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 100
+	.short	191, 100
+	.short	191, 100
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 100
+	.short	191, 110
+	.short	191, 70
+	.short	191, 110
+	.short	191, 105
+	.short	191, 110
+	.short	191, 115
+	.short	191, 110
+	.short	191, 110
+	.short	191, 110
+	.short	191, 70
+	.short	191, 115
+	.short	191, 100
+	.short	191, 100
+	.short	191, 110
+	.short	191, 110
+	.short	191, 70
+	.short	191, 110
+	.short	191, 110
+	.short	300, 100
+	.short	300, 127
+	.short	300, 127
+	.short	300, 127
+	.short	300, 127
+	.short	300, 127
+	.short	300, 100
+	.short	300, 80
+	.short	300, 127
+	.short	300, 127
+	.short	300, 127
+	.short	300, 127
+	.short	300, 80
+	.short	191, 110
+	.short	190, 127
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	23, 80
+	.short	23, 80
+	.short	24, 80
+	.short	25, 80
+	.short	26, 100
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	22, 90
+	.short	351, 90
+	.short	352, 90
+	.short	353, 90
+	.short	354, 90
+	.short	355, 90
+	.short	356, 90
+	.short	357, 90
+	.short	358, 90
+	.short	359, 90
+	.short	360, 90
+	.short	361, 90
+	.short	362, 96
+	.short	363, 90
+	.short	364, 90
+	.short	365, 90
+	.short	366, 90
+	.short	367, 90
+	.short	368, 90
+	.short	369, 90
+	.short	370, 90
+	.short	371, 86
+	.short	372, 90
+	.short	373, 90
+	.short	374, 90
+	.short	375, 90
+	.short	376, 90
+	.short	377, 90
+	.short	378, 90
+	.short	379, 100
+	.short	380, 80
+	.short	381, 86
+	.short	382, 96
+	.short	383, 90
+	.short	384, 90
+	.short	385, 90
+	.short	386, 76
+	.short	387, 90
+	.short	388, 90
+	.short	389, 90
+	.short	390, 90
+	.short	391, 90
+	.short	392, 90
+	.short	393, 84
+	.short	394, 90
+	.short	394, 90
+	.short	397, 80
+	.short	400, 102
+	.short	401, 94
+	.short	402, 100
+	.short	403, 100
+	.short	390, 100
+	.short	404, 100
+	.short	404, 100
+	.short	404, 100
+	.short	352, 90
+	.short	352, 90
+	.short	382, 96
+	.short	405, 100
+	.short	352, 90
+	.short	367, 90
+	.short	351, 90
+	.short	433, 80
+	.short	366, 90
+	.short	393, 84
+	.short	434, 84
+	.short	435, 84
+	.short	385, 96
+	.short	431, 80
+	.short	377, 90
+	.short	377, 90
+	.short	364, 90
+	.short	430, 90
+	.short	354, 90
+	.short	379, 92
+	.short	432, 90
+
+	.global	__total_vgrp_n
+	.equ	__total_vgrp_n, 437
+
+	.global	app_vgrp_table
+app_vgrp_table:
+	.word	voicegroup000
+	.word	voicegroup001
+	.word	voicegroup002
+	.word	0
+	.word	0
+	.word	voicegroup005
+	.word	voicegroup006
+	.word	voicegroup007
+	.word	voicegroup008
+	.word	voicegroup009
+	.word	voicegroup010
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	voicegroup022
+	.word	voicegroup023
+	.word	voicegroup024
+	.word	voicegroup025
+	.word	voicegroup026
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	voicegroup190
+	.word	voicegroup191
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	voicegroup200
+	.word	voicegroup201
+	.word	voicegroup202
+	.word	voicegroup203
+	.word	voicegroup204
+	.word	voicegroup205
+	.word	voicegroup206
+	.word	voicegroup207
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	voicegroup300
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	voicegroup350
+	.word	voicegroup351
+	.word	voicegroup352
+	.word	voicegroup353
+	.word	voicegroup354
+	.word	voicegroup355
+	.word	voicegroup356
+	.word	voicegroup357
+	.word	voicegroup358
+	.word	voicegroup359
+	.word	voicegroup360
+	.word	voicegroup361
+	.word	voicegroup362
+	.word	voicegroup363
+	.word	voicegroup364
+	.word	voicegroup365
+	.word	voicegroup366
+	.word	voicegroup367
+	.word	voicegroup368
+	.word	voicegroup369
+	.word	voicegroup370
+	.word	voicegroup371
+	.word	voicegroup372
+	.word	voicegroup373
+	.word	voicegroup374
+	.word	voicegroup375
+	.word	voicegroup376
+	.word	voicegroup377
+	.word	voicegroup378
+	.word	voicegroup379
+	.word	voicegroup380
+	.word	voicegroup381
+	.word	voicegroup382
+	.word	voicegroup383
+	.word	voicegroup384
+	.word	voicegroup385
+	.word	voicegroup386
+	.word	voicegroup387
+	.word	voicegroup388
+	.word	voicegroup389
+	.word	voicegroup390
+	.word	voicegroup391
+	.word	voicegroup392
+	.word	voicegroup393
+	.word	voicegroup394
+	.word	voicegroup395
+	.word	voicegroup396
+	.word	voicegroup397
+	.word	voicegroup398
+	.word	voicegroup399
+	.word	voicegroup400
+	.word	voicegroup401
+	.word	voicegroup402
+	.word	voicegroup403
+	.word	voicegroup404
+	.word	voicegroup405
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	0
+	.word	voicegroup430
+	.word	voicegroup431
+	.word	voicegroup432
+	.word	voicegroup433
+	.word	voicegroup434
+	.word	voicegroup435
+	.word	voicegroup436
+
+	.global	pbymidi_swi
+	.equ	pbymidi_swi, 0
+	.global	pbymidi_vg
+	.equ	pbymidi_vg, 0
+	.global	pbymidi_mv
+	.equ	pbymidi_mv, 127
+	.global	pbymidi_pr
+	.equ	pbymidi_pr, 255
+	.global	pbymidi_tm
+	.equ	pbymidi_tm, 150
+
+	.global	seg_rodata_top
+	.equ	seg_rodata_top,.
+
+	.section .rodata.end
+	.global	seg_rodata_end
+	.equ	seg_rodata_end,.
+	.word	0
+
+	.end
